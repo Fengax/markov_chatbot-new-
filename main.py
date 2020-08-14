8a6488184abeeed1a75f807c6e9ba139f8926791
Fengax <857514.leofeng@gmail.com>
2020-08-14 22:04:22 +1200

    Added loadtext function in main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import random
 
-
-
 class markovBot:
     def __init__(self):
         self.end = 857514
-        self.markov_dict = [["hello there", "how"], ["there how", "are"], ["how are", "you"], ["are you", self.end],
-                       ["hello there", "why"], ["there why", "how"], ["why how", "are"]]
+        self.markov_dict = []
 
-    def run(self):
+    def loadtext(self, textfile):
+        file = open(textfile, "r")
+        for phrase_couple in file.readlines():
+            split_array = phrase_couple.split()
+            if split_array[2] == "857514":
+                self.markov_dict.append([split_array[0] + " " + split_array[1], self.end])
+            else:
+                self.markov_dict.append([split_array[0] + " " + split_array[1], split_array[2]])
+
+    def run(self, textfile):
         while True:
             chat = input()
             chat_lower = chat.lower()
@@ -17,15 +23,19 @@ class markovBot:
                 start_phrase = chat_lower.split()[0] + " " + chat_lower.split()[1]
             except:
                 print("Unable to start, possibly starting sentence too short, at least 2 words.")
+                continue
 
             response = start_phrase
+            file = open(textfile, "a")
 
             for i in range(0, len(chat_lower.split()) - 1):
                 this_phrase = chat_lower.split()[i] + " " + chat_lower.split()[i + 1]
                 if i == len(chat_lower.split()) - 2:
                     self.markov_dict.append([this_phrase, self.end])
+                    file.write(this_phrase + " " + str(self.end) + "\n")
                 else:
                     self.markov_dict.append([this_phrase, chat_lower.split()[i + 2]])
+                    file.write(this_phrase + " " + chat_lower.split()[i + 2] + "\n")
 
             while True:
                 index_array = []
@@ -44,4 +54,5 @@ class markovBot:
 
 
 bot = markovBot()
-bot.run()
+bot.loadtext("text.txt")
+bot.run("text.txt")
